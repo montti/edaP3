38729cb472c0644938d2f9c697249c4227b25b25
jonathanavc <jonvenega@gmail.com>
2022-11-03 14:27:35 -0300

    Update FMIndex.h

--- FMIndex.h
+++ FMIndex.h
@@ -4,6 +4,10 @@ using namespace std;
 
 class FMIndex {
 	private:
+		vector<char> F;
+		vector<char> L;
+		vector<int> SA_Sample;
+		vector<int> Checkpoits; //no es necesaria para realizar count
 		string _t;
 	public:
 		FMIndex(const string &s);
